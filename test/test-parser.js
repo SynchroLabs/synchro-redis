d888b82ad61ab5260f4274f8c91e3eb17ccc2883
Blake Ramsdell <blake@synchro.io>
2014-08-28 19:01:08 -0700

    Fixed broken tests to be pending tests.

--- test/test-parser.js
+++ test/test-parser.js
@@ -3,47 +3,27 @@ var assert = require('assert')
 // http://redis.io/topics/protocol
 
 describe('Parser', function() {
-	it('should exist', function() {
-		assert(Parser)
-	})
+	it('should exist')
 
-	it('should parse simple strings', function() {
-		assert(false)
-	})
+	it('should parse simple strings')
 
-	it('should parse errors', function() {
-		assert(false)
-	})
+	it('should parse errors')
 
-	it('should parse integers', function() {
-		assert(false)
-	})
+	it('should parse integers')
 
 	describe('bulk string handling', function() {
-		it('should parse bulk strings', function() {
-			assert(false)
-		})
-		it('should parse empty bulk strings', function() {
-			assert(false)
-		})
-		it('should parse null bulk strings', function() {
-			assert(false)
-		})
+		it('should parse bulk strings')
+
+		it('should parse empty bulk strings')
+
+		it('should parse null bulk strings')
 	})
 
 	describe('array handling', function() {
-		it('should parse arrays', function() {
-			assert(false)
-		})
-		it('should parse empty arrays', function() {
-			assert(false)
-		})
-		it('should parse null arrays', function() {
-			assert(false)
-		})
-		it('should handle null elements in arrays', function() {
-			assert(false)
-		})
+		it('should parse arrays')
+		it('should parse empty arrays')
+		it('should parse null arrays')
+		it('should handle null elements in arrays')
 	})
 })
 
