3939d9fce66fae5312def31cad4ef2d0507b90cc
Blake Ramsdell <blake@synchro.io>
2014-08-30 12:06:48 -0700

    Added object setting.

--- test/test-connection.js
+++ test/test-connection.js
@@ -1,3 +1,5 @@
+var assert = require('assert')
+var resp = require('../resp')
 var net = require('net')
 
 // http://lostechies.com/derickbailey/2012/08/17/asynchronous-unit-tests-with-mocha-promises-and-winjs/
@@ -9,7 +11,26 @@ describe('Redis connection', function() {
 		})
 	})
 	it('should authenticate')
-	it('should set objects')
+	it('should set objects', function(done) {
+		var value = new Date().toString()
+
+		var command = [ "set", "unittest", value ]
+		var parserState = {}
+
+		var connection = net.connect({ port: 6379}, function() {
+			connection.write(resp.encode_redis(command))
+		})
+
+		connection.on('data', function(data) {
+			resp.parse(data, 0, parserState)
+
+			if ('completeType' in parserState)
+			{
+				assert.equal(parserState.completeType, "OK")
+				done()
+			}
+		})
+	})
 	it('should get objects')
 	it('should recover if the connection dies')
 })
