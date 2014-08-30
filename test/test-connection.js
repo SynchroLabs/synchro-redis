dd769bfdbd4bf1542986b520ac6009250ea86a57
Blake Ramsdell <blake@synchro.io>
2014-08-30 11:51:51 -0700

    First connection test.

--- test/test-connection.js
+++ test/test-connection.js
@@ -1,5 +1,13 @@
+var net = require('net')
+
+// http://lostechies.com/derickbailey/2012/08/17/asynchronous-unit-tests-with-mocha-promises-and-winjs/
+
 describe('Redis connection', function() {
-	it('should connect')
+	it('should connect', function(done) {
+		var connection = net.connect({ port: 6379}, function() {
+			done()
+		})
+	})
 	it('should authenticate')
 	it('should set objects')
 	it('should get objects')
