c02553f30aa6ef40bb09ce034038db13c89705dc
Blake Ramsdell <blake@synchro.io>
2014-08-30 11:33:58 -0700

    Added connection tests.

---
+++ test/test-connection.js
@@ -0,0 +1,7 @@
+describe('Redis connection', function() {
+	it('should connect')
+	it('should authenticate')
+	it('should set objects')
+	it('should get objects')
+	it('should recover if the connection dies')
+})
