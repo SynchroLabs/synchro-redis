29a736868e8b0859c14b5f5e3469b5f3c36f0927
Blake Ramsdell <blake@synchro.io>
2014-08-29 18:12:59 -0700

    Yanked some redundant tests.

--- test/test-generator.js
+++ test/test-generator.js
@@ -5,14 +5,6 @@ describe('RESP generator', function() {
 	it('should exist', function() {
 		assert(resp.encode)
 	})
-	it('should generate simple strings', function() {
-		assert.equal(resp.encode('a'), "+a\r\n")
-	})
-	it('should generate integers', function() {
-		assert.equal(resp.encode(1234), ":1234\r\n")
-	})
-	it('should generate bulk strings')
-	it('should generate arrays')
 	describe('automatic type selection', function() {
 		it('should use integer for Javascript number', function() {
 			assert.equal(resp.encode(1234), ":1234\r\n")
