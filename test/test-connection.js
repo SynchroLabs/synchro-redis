a13f442222f47be8611432b23aa1fbced42c724e
Blake Ramsdell <blake@synchro.io>
2014-08-30 23:11:42 -0700

    Added mock redis server.

--- test/test-connection.js
+++ test/test-connection.js
@@ -4,11 +4,60 @@ var net = require('net')
 
 // http://lostechies.com/derickbailey/2012/08/17/asynchronous-unit-tests-with-mocha-promises-and-winjs/
 
+function RedisServer(running)
+{
+	var objects = {}
+
+	this.server = net.createServer(function(connection) {
+		var parserState = {}
+
+		connection.on('data', function(data) {
+			var offset = 0
+
+		    	while (offset < data.length)
+			{
+				offset = resp.parse(data, offset, parserState)
+
+				if ('completeType' in parserState)
+				{
+					var returnData = ""
+
+					switch (parserState.completeType[0].toString())
+					{
+						case "set":
+							objects[parserState.completeType[1]] = parserState.completeType[2]
+							returnData = "OK"
+							break;
+
+						case "get":
+							returnData = objects[parserState.completeType[1]]
+							break;
+					}
+
+			    		parserState = {}
+
+					connection.write(resp.encode_redis(returnData))
+				}
+		    	}			
+		})
+	})
+	this.server.listen(0, running)
+}
+
 describe('Redis connection', function() {
 	var value = new Date().toString()
+	var port = 0 // 6379
+	var server = null
+
+	before(function(done) {
+		server = new RedisServer(function() {
+			port = server.server.address().port
+			done()
+		})
+	})
 
 	it('should connect', function(done) {
-		var connection = net.connect({ port: 6379}, function() {
+		var connection = net.connect({ port: port}, function() {
 			done()
 		})
 	})
@@ -17,7 +66,7 @@ describe('Redis connection', function() {
 		var command = [ "set", "unittest", value ]
 		var parserState = {}
 
-		var connection = net.connect({ port: 6379}, function() {
+		var connection = net.connect({ port: port}, function() {
 			connection.write(resp.encode_redis(command))
 		})
 
@@ -42,7 +91,7 @@ describe('Redis connection', function() {
 		var command = [ "get", "unittest" ]
 		var parserState = {}
 
-		var connection = net.connect({ port: 6379}, function() {
+		var connection = net.connect({ port: port}, function() {
 			connection.write(resp.encode_redis(command))
 		})
 
